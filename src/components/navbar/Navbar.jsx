78211c56367047532458b9d8f46f2c4df093c956
Bảo Võ Hoàng <andy@Baos-MacBook-Pro.local>
2023-11-09 09:08:48 +0100

    second commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -39,6 +39,7 @@ const Navbar = () => {
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign up</button>
+        <button type="button">Sign up</button>
       </div>
       <div className="gpt3__navbar-menu">
         {toggleMenu ? (
